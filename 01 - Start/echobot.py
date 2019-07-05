17b172b32c552ff49879a25dc97b670f77f379d1
Artem Pazych <pazzzych@gmail.com>
2019-07-05 22:41:47 +0300

    Bot gets your last message and send you it's text.

---
+++ 01 - Start/echobot.py
@@ -0,0 +1,38 @@
+import json 
+import requests
+
+TOKEN = '832771187:AAEGItC7eWBHG19QEhllDB8swvtXquMuQBU'
+URL = 'https://api.telegram.org/bot{}/'.format(TOKEN)
+
+def get_url(url):
+    response = requests.get(url)
+    content = response.content.decode('utf-8')
+    print('CONTENT:\n', content)
+    return content
+
+def get_json_form_url(url):
+    content = get_url(url)
+    js = json.loads(content)
+    print('JS:\n', js)
+    return js
+
+def get_updates():
+    url = URL + 'getUpdates'
+    js = get_json_form_url(url)
+    print(js)
+    return js
+
+def get_last_chat_id_and_text(updates):
+    num_updates = len(updates['result'])
+    last_update = num_updates - 1
+    text = updates['result'][last_update]['message']['text']
+    chat_id = updates['result'][last_update]['message']['chat']['id']
+    return text, chat_id
+
+def send_message(text, chat_id):
+    url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
+    get_url(url)
+
+
+text, chat = get_last_chat_id_and_text(get_updates())
+send_message(text, chat)
