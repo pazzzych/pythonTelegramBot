0d7c8f6272e8d5b7762fc8e72ba8e6690a340f42
Artem Pazych <pazzzych@gmail.com>
2019-07-06 10:35:03 +0300

    Bot adds new item in database if it's not already exists, and delete if it's already exists.

--- 02 - To do Bot/dbhelper.py
+++ 02 - To do Bot/dbhelper.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-class DBHElper:
+class DBHelper:
     # takes a database name 
     # (by default store our data in a file called todo.sqlite) 
     # and creates a database connection
@@ -11,26 +11,26 @@ class DBHElper:
     # creates a new table called items in our database 
     # This table has one column (called description)
     def setup(self):
-        stmt = 'CREATE TABLE IF NOT EXISTS items (desctiption text)'
+        stmt = "CREATE TABLE IF NOT EXISTS items (description text)"
         self.conn.execute(stmt)
         self.conn.commit()
 
     # takes the text for the item and inserts it into our database table
     def add_item(self, item_text):
-        stmt = 'INSERT INTO items (description) VALUES (?)'
+        stmt = "INSERT INTO items (description) VALUES (?)"
         args = (item_text, )
         self.conn.execute(stmt, args)
         self.conn.commit()
     
     # takes the text for an item and removes it from the database
     def delete_item(self, item_text):
-        stmt = 'DELETE FORM items WHERE description = (?)'
+        stmt = "DELETE FROM items WHERE description = (?)"
         args = (item_text, )
         self.conn.execute(stmt, args)
         self.conn.commit()
 
     # returns a list of all the items in our database
     def get_items(self):
-        stmt = 'SELECT description FROM items'
+        stmt = "SELECT description FROM items"
         return [x[0] for x in self.conn.execute(stmt)]
 
