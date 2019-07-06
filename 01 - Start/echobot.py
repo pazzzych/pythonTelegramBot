0104fdad9a7973492dd98e864f2a493a77536f83
Artem Pazych <pazzzych@gmail.com>
2019-07-06 09:14:52 +0300

    With the timeout, bot will only initiate a new check every 100 seconds, or whenever a new message is received.

--- 01 - Start/echobot.py
+++ 01 - Start/echobot.py
@@ -17,11 +17,19 @@ def get_json_form_url(url):
     print('JS:\n', js)
     return js
 
-def get_updates():
-    url = URL + 'getUpdates'
+def get_updates(offset=None):
+    url = URL + 'getUpdates?timeout=100'
+    if offset:
+        url += '&offset={}'.format(offset)
     js = get_json_form_url(url)
     print(js)
     return js
+
+def get_last_update_id(updates):
+    update_ids = []
+    for update in updates['result']:
+        update_ids.append(int(update['update_id']))
+    return max(update_ids)
 
 def get_last_chat_id_and_text(updates):
     num_updates = len(updates['result'])
@@ -30,18 +38,26 @@ def get_last_chat_id_and_text(updates):
     chat_id = updates['result'][last_update]['message']['chat']['id']
     return text, chat_id
 
+def echo_all(updates):
+    for update in updates['result']:
+        try:
+            text = update['message']['text']
+            chat = update['message']['chat']['id']
+            send_message(text, chat)
+        except Exception as e:
+            print(e)
+
 def send_message(text, chat_id):
     url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
     get_url(url)
 
-
 def main():
-    last_textchat = (None, None)
+    last_update_id = None
     while True:
-        text, chat = get_last_chat_id_and_text(get_updates())
-        if (text, chat) != last_textchat:
-            send_message(text, chat)
-            last_textchat = (text, chat)
+        updates = get_updates(last_update_id)
+        if len(updates['result']) > 0:
+            last_update_id = get_last_update_id(updates) + 1
+            echo_all(updates)
         time.sleep(0.5)
 
 
